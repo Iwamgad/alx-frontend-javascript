eead0b74e768ec27efdc085dc69676319de6f605
Iwamgad <dagmawiharry2@gmail.com>
2023-01-29 14:53:53 +0000

    I edited some files

---
+++ 0x00-ES6_basic/test/4-main.js
@@ -0,0 +1,4 @@
+import returnHowManyArguments from './Trial.js';
+
+console.log(returnHowManyArguments("one"));
+console.log(returnHowManyArguments("one", "two", 3, "4th"));
---
+++ 0x00-ES6_basic/test/Trial.js
@@ -0,0 +1,19 @@
+/*
+export default function returnHowManyArguments(...args) {
+  let count = 0;
+  for (let arg of args) count += 1;
+  return count;  
+}
+
+
+export default function concatArrays(array1, array2, string) {
+    let totalArray = array1.concat(array2);
+
+    for (let i = 0; i < string.length; i++){
+	totalArray = totalArray.concat(string[i]);
+    }
+    
+
+    return totalArray;
+}
+*/
