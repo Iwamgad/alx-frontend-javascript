a6548780ec966bf5f69761a72019f9d11dbd9e88
Iwamgad <dagmawiharry2@gmail.com>
2023-01-30 18:00:08 +0000

    I added the file again

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,28 +1,8 @@
 export default function getListStudents(){
     
-    let retArray = [];
-
-    const obj1 = {
-
-        id: 1,
-        firstName: "Guillaume",
-        location: "San Francisco"
-    };
-
-    const obj2 = {
-        id: 2,
-        firstName: "James",
-        location: "San Francisco"
-    };
-
-    const obj3 = {
-        id: 5,
-        firstName: "Serena",
-        location: "San Francisco"
-    };
-
-    retArray.push(obj1, obj2, obj3);
-
-
-    return retArray;
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
