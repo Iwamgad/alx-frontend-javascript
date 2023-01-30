6a7cc790d00b3387552902ca59465253723d7010
Iwamgad <dagmawiharry2@gmail.com>
2023-01-30 17:53:52 +0000

    I did task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,28 @@
+function getListStudents(){
+    
+    let retArray = [];
+
+    const obj1 = {
+
+        id: 1,
+        firstName: "Guillaume",
+        location: "San Francisco"
+    };
+
+    const obj2 = {
+        id: 2,
+        firstName: "James",
+        location: "San Francisco"
+    };
+
+    const obj3 = {
+        id: 5,
+        firstName: "Serena",
+        location: "San Francisco"
+    };
+
+    retArray.push(obj1, obj2, obj3);
+
+
+    return retArray;
+}
